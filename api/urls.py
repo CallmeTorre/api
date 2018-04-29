e432fe6f8b688829c1168d8f41a97d06b9e61e7f
CallmeTorre <alexis_torreblanca@outlook.com>
2018-04-29 16:58:52 -0500

    [Added] requirements.txt and Api schema

--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import url, include
+from rest_framework import routers, schemas
 from restaurants import urls as r_urls
+
+schema_view = schemas.get_schema_view(title = 'Restaurants')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('restaurants/', include(r_urls)),
+    path('schema/', schema_view),
 ]
